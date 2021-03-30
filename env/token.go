8bf6650cc91abf88911de08994f0d5518ca341ea
nickmancari <nickjmancari@gmail.com>
2021-03-30 21:56:00 +0000

    token flag input

--- env/token.go
+++ env/token.go
@@ -1,7 +1,28 @@
-package main
+package token
 
-import
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+)
 
-func TokenInTake() {
+\\func main() {
+\\	tokenFlag := flag.String("token", "", "Digital Ocean API Token")
+\\	flag.Parse()
+\\	CreateTokenFile(*tokenFlag)
+\\
+\\	fmt.Println("test successful")
+\\}
 
+func CreateTokenFile(tokenInput string) {
+
+	file, err := os.Create(".token")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	file.Write([]byte(tokenInput + "\n"))
+	file.Close()
 }
+
