f344ffbc2ab5576257a3744a6fe0112ab98084d1
nickmancari <nickjmancari@gmail.com>
2021-04-08 18:34:41 +0000

    starting ssh functionality

--- tools/convert.go
+++ tools/convert.go
@@ -1,8 +1,13 @@
 package convert
 
 import (
+	
+	"gitub.com/nickmancari/gocean/env"
 
 )
+
+var apiToken = token.ReadTokenFile(".token")
+var apiGetAddress = "https://api.digitalocean.com/v2/droplets?page=1&per_page=1"
 
 func ToDropletID(f string) {
 	request, err := http.NewRequest("GET", apiGetAddress, nil)
@@ -12,4 +17,6 @@ func ToDropletID(f string) {
 
 	request.Header.Add("Content-Type", "application/json")
 	request.Header.Add("Authorization", "Bearer ", +apiToken)
+
+
 }
