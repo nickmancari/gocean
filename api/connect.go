ff3aec90871cda51358042ec4fc08a6d8d8715dd
nickmancari <nickjmancari@gmail.com>
2021-04-15 13:42:34 +0000

    better structure for convert func

--- api/connect.go
+++ api/connect.go
@@ -37,5 +37,29 @@ func Connection(method string, url string, info io.Reader) interface{} {
 		fmt.Println(err)
 	}
 	return r
+}
+
+func ConvertConnection(method string, url string, info io.Reader) []byte {
+
+	request, err := http.NewRequest(method, url, info)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	request.Header.Add("Content-Type", "application/json")
+	request.Header.Add("Authorization", "Bearer "+Token)
+
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer response.Body.Close()
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return body
 
 }
--- tools/convert.go
+++ tools/convert.go
@@ -3,15 +3,12 @@ package convert
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"net/http"
 	"strings"
 
-	token "github.com/nickmancari/gocean/env"
+	connect "github.com/nickmancari/gocean/api"
 )
 
 var apiGetAddress = "https://api.digitalocean.com/v2/droplets"
-var Token = token.ReadTokenFile(".token")
 
 type droplets struct {
 	Droplets []info `json:"droplets"`
@@ -23,30 +20,12 @@ type info struct {
 }
 
 func ToID(s string) (string, error) {
-	request, err := http.NewRequest("GET", apiGetAddress, nil)
-	if err != nil {
-		fmt.Println("HTTP Request Error: ", err)
-	}
-
-	request.Header.Add("Content-Type", "application/json")
-	request.Header.Add("Authorization", "Bearer "+Token)
-
-	client := &http.Client{}
-	response, err := client.Do(request)
-	if err != nil {
-		fmt.Println("Client Error: ", err)
-	}
-	defer response.Body.Close()
-
-	body, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		fmt.Println("ReadAll Error: ", err)
-	}
+	body := connect.ConvertConnection("GET", apiGetAddress, nil)
 
 	dropletStruct := droplets{}
 	er := json.Unmarshal(body, &dropletStruct)
 	if er != nil {
-		fmt.Println("Unmarshall Error: ", err)
+		fmt.Println("Unmarshall Error: ", er)
 	}
 
 	var r int
