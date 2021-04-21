25b1993d5327f2b81f98ff660bbe76cbb3e201da
nickmancari <nickjmancari@gmail.com>
2021-04-21 20:46:34 +0000

    formatted response

--- data/response.go
+++ data/response.go
@@ -10,9 +10,26 @@ type droplet struct {
 }
 
 type system struct {
-	ID     int    `json:"id"`
-	Name   string `json:"name"`
-	Status string `json:"status"`
+	ID     	int    		`json:"id"`
+	Name   	string 		`json:"name"`
+	Status 	string 		`json:"status"`
+	Image	imageInfo	`json:"image"`
+	Net	network		`json:"networks"`
+}
+
+type imageInfo struct {
+	Name	string		`json:"name"`
+	Distro	string		`json:"distribution"`
+}
+
+type network struct {
+	V4	[]netDetail	`json:"v4"`
+}
+
+type netDetail struct {
+	IP	string		`json:"ip_address"`
+	Netmask	string		`json:"netmask"`
+	Gateway	string		`json:"gateway"`
 }
 
 func ToStructuredResponse(b []byte) (interface{}, error) {
@@ -21,7 +38,7 @@ func ToStructuredResponse(b []byte) (interface{}, error) {
 	if er != nil {
 		fmt.Println("Unmarshal Error ", er)
 	}
-	r, _ := fmt.Printf("\n|%6s|\n\n|ID: %6d||Status: %6s|\n\n", dropletStruct.Droplet.Name, dropletStruct.Droplet.ID, dropletStruct.Droplet.Status)
+	r, _ := fmt.Printf("\n|%6s|\n\n|ID: %6d||Status: %6s||Distro: %6s||Ip Address: %6s|\n\n", dropletStruct.Droplet.Name, dropletStruct.Droplet.ID, dropletStruct.Droplet.Status, dropletStruct.Droplet.Image, dropletStruct.Droplet.Net.V4[0].IP)
 
 	return r, nil
 
